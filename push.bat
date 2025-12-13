cc785b9c4c414d0a99a7c9e2d43daac6c8789118
买一座空白 <528525454@qq.com>
2025-12-13 14:59:20 +0800

    周六 2025/12/13 14:59:20.88

--- push.bat
+++ push.bat
@@ -1,7 +1,6 @@
 @echo off
 :: 防止中文乱码，切换编码页到 UTF-8
 chcp 65001 >nul
-
 echo ==============================
 echo 正在提交笔记...
 echo ==============================
@@ -10,8 +9,4 @@ call git add .
 set "timestamp=%date% %time%"
 call git commit -m "%timestamp%"
 call git push origin main
-echo.
-echo ==============================
-echo 提交成功！
-echo 若想查看请访问 https://github.com/ljh-sys/LJH_Node
-echo ==============================
+pause
